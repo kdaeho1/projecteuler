216953666f9b81b1f1c6936d652b02caba536a2e
Daeho  Kang <47322032+kdaeho1@users.noreply.github.com>
2024-02-08 20:18:23 +0900

    Add files via upload

--- Q11.py
+++ Q11.py
@@ -3,31 +3,31 @@ ADJ_LEN = 4
 GRD_LEN = 20
 
 f = open("grid.txt","r")
-grid = [[0]*GRD_LEN]*GRD_LEN
-numgrid = [[0]*GRD_LEN]*GRD_LEN
+grid = [[]*GRD_LEN]*GRD_LEN
 counter = 0
 product = 0
 
 for line in f:
     grid[counter] = line.split()
+    grid[counter] = grid[counter]
     counter += 1
+    
+grid = [[int(i) for i in line] for line in grid]
 #print(grid)
 
 for i in range(GRD_LEN):
-    for j in range(GRD_LEN):
-        numgrid[i][j] = int(grid[i][j])
-#print(numgrid)
-
-for i in range(GRD_LEN):
     for j in range(GRD_LEN - ADJ_LEN+1):
-        if product < math.prod(numgrid[i][j:j+ADJ_LEN]):
-            product = math.prod(numgrid[i][j:j+ADJ_LEN])
-
+        tmp = 1
+        for k in range(ADJ_LEN):
+            tmp *= grid[i][j+k]
+        if product < tmp:
+            product = tmp
+            
 for i in range(GRD_LEN - ADJ_LEN + 1):
     for j in range(GRD_LEN):
         tmp = 1
         for k in range(ADJ_LEN):
-            tmp *= numgrid[i+k][j]
+            tmp *= grid[i+k][j]
         if product < tmp:
             product = tmp
 
@@ -35,7 +35,7 @@ for i in range(GRD_LEN - ADJ_LEN+1):
     for j in range(GRD_LEN - ADJ_LEN+1):
         tmp = 1
         for k in range(ADJ_LEN):
-            tmp *= numgrid[i+k][j+k]
+            tmp *= grid[i+k][j+k]
         if product < tmp:
             product = tmp
                 
@@ -43,9 +43,9 @@ for i in range(ADJ_LEN, GRD_LEN):
     for j in range(GRD_LEN - ADJ_LEN+1):
         tmp = 1
         for k in range(ADJ_LEN):
-            tmp *= numgrid[i-k][j+k]
+            tmp *= grid[i-k][j+k]
         if product < tmp:
             product = tmp
-                
+
 print(product)
                 
