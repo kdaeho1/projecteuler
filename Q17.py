793484fef62ac7a9da72167dc1f60f40d55b87b8
Daeho  Kang <47322032+kdaeho1@users.noreply.github.com>
2024-02-11 15:40:48 +0900

    Add files via upload

--- Q17.py
+++ Q17.py
@@ -1,5 +1,7 @@
 def number_letter_count_ones(num): #one two three four five six seven eight nine ten
-    if num == 1 or num == 2 or num == 6 or num == 10: #one two six ten
+    if num == 0:
+        return 0
+    elif num == 1 or num == 2 or num == 6: #one two six ten
         return 3
     elif num == 4 or num == 5 or num == 9: #four five nine
         return 4
@@ -16,15 +18,21 @@ def number_letter_count_teens(num): #eleven twelve thirteen fourteen fifteen six
     else:
         return 9
 
-def number_letter_count_tens(num): #ten twenty thirty fourty fifty sixty seventy eighty ninety
-    if num == 50 or num == 60:
+def number_letter_count_tens(num): #ten twenty thirty forty fifty sixty seventy eighty ninety
+    if num == 0:
+        return 0
+    elif num == 40 or num == 50 or num == 60:
         return 5
-    elif num == 20 or num == 30 or num == 40 or num == 80 or num == 90:
+    elif num == 20 or num == 30 or num == 80 or num == 90:
         return 6
     elif num == 10:
         return 3
     else:
         return 7
+        
+def number_letter_count_hundreds(num):
+    #print(number_letter_count_ones(num/100)+7)
+    return number_letter_count_ones(num/100) + 7
 
 def number_letter_count(num):
     ones = num%10
@@ -33,22 +41,33 @@ def number_letter_count(num):
     hundreds = num%1000
     hundreds -= hundreds%100
     #print(ones, tens, hundreds)
+    
+    if num == 1000:
+        return 11
+    
     if num < 10:
         return number_letter_count_ones(num)
-    elif num < 20:
+    elif num < 20 and num > 10:
         return number_letter_count_teens(num)
     elif num < 100:
-        return number_letter_count_tens(num)
-    
-    if num == 1000: #case: 1000
-        # one thousand = 11 letters
-        return 11
-    elif 100 < num: #case: 100 to 999
-        if num % 100 == 0: #case: one hundred, two hundred, etc
-            return 0
+        return number_letter_count_tens(tens) + number_letter_count_ones(ones)
+    elif 100 <= num: #case: 100 to 999
+        if num % 100 == 0: #case: x hundred
+            return number_letter_count_hundreds(hundreds)
+        elif tens == 10 and ones != 0: #case: x hundred + and + teen number
+            return number_letter_count_hundreds(hundreds) + number_letter_count_teens(num%100) + 3
+        else: #case: x hundred + and + number
+            return number_letter_count_hundreds(hundreds) + number_letter_count_tens(tens) + number_letter_count_ones(ones) + 3
     return 0
     
-number_letter_count(525)
+ans = 0
+
+print(number_letter_count(342))
+
+for i in range(1, 1001):
+    #print(i)
+    ans += number_letter_count(i)
+print(ans)
             
             
         
