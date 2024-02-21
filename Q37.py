664b634d68adbe4fc17511090753d2e0bebee3a7
Daeho  Kang <47322032+kdaeho1@users.noreply.github.com>
2024-02-21 23:26:52 +0900

    Add files via upload

--- Q37.py
+++ Q37.py
@@ -6,24 +6,38 @@ def primes(n):
     return [2] + [2*i+1 for i in range(1,int(n/2)) if sieve[i]]
 
 def truncate(n):
-    primeList = primes(1000000)
+    primeList = primes(n)
     ans = 0
-    count = 0
-    while count < 11:
-        for prime in primeList:
-            digits = len(str(prime))
-            count = 0
-            for digit in range(1,digits):
-                if int(str(prime)[digit::]) in primeList and int(str(prime)[:digit:]) in primeList:
-                    count += 1
-                    
-            if digits == count+1:
-                ans += prime
-                print(prime)
+    found = 0
+    for prime in primeList:
+        if prime < 10:
+            digits = 1
+        elif prime < 100:
+            digits = 2
+        elif prime < 1000:
+            digits = 3
+        elif prime < 10000:
+            digits = 4
+        elif prime < 100000:
+            digits = 5
+        elif prime < 1000000:
+            digits = 6
+            
+        count = 0
+        for digit in range(1,digits):
+            if int(str(prime)[digit::]) in primeList and int(str(prime)[:digit:]) in primeList:
+                count += 1
+        
+        if digits == 1:
+            continue
+        elif digits == count+1:
+            ans += prime
+            print(prime)
+            found += 1
+            
+        if found == 11:
+            break
         
     return ans
 
 print(truncate(1000000))
-        
-        
-        
