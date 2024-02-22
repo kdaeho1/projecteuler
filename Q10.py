fa4d5361690c4b83dd2cb29f9aa3a897fd071cbc
Daeho  Kang <47322032+kdaeho1@users.noreply.github.com>
2024-02-22 23:33:33 +0900

    Update Q10.py

--- Q10.py
+++ Q10.py
@@ -1,9 +1,8 @@
 def primes(n):
-    sieve = [True] * int(n/2)
-    for i in range(3,int(n**0.5+1),2):
-        if sieve[int(i/2)]:
-            sieve[int(i*i/2)::i] = [False] * int((n-i*i-1)/(2*i)+1)
-    return [2] + [2*i+1 for i in range(1,int(n/2)) if sieve[i]]
-
-
+    sieve = [True] * int(n/2) #all even numbers are not primes (except 2)
+    for i in range(3,int(n**0.5+1),2): #3, 5, 7, ...
+        if sieve[int(i/2)]: #case: i=3, sieve[1] (representing 3) is true, then 
+            sieve[int((i*i-1)/2)::i] = [False] * int((n-i*i-1)/(2*i)+1) #sieve[4], sieve[7], ... are representing odd multiples of 3 (9, 15, 21, ...)
+    return [2] + [2*i+1 for i in range(1,int(n/2)) if sieve[i]] #return sieved true (primes)
+    
 print(sum(primes(2000000)))
