6e1090be05bcdec638cc5364f79df3da9b828303
Daeho  Kang <47322032+kdaeho1@users.noreply.github.com>
2024-02-22 22:43:45 +0900

    Add files via upload

--- Q39.py
+++ Q39.py
@@ -1,9 +1,22 @@
-#cond 1: a + b + c <= 1000
-#cond 2: a^2 + b^2 = c^2
-#cond 1-1: a < b < c
-#cond 1-2: c <= 500, a + b > c
+def abcbalancer(p):
+    count = 0
+    for b in range(int(p/3), int(p/2)):
+        for c in range(b+1, int(p/2)+1):
+            a = p-b-c
+            if a**2 + b**2 == c**2:
+                print(a, b, c)
+                count += 1
+    return count
 
+def solution(p):
+    max = 0
+    for i in range(int(p/2), p+1):
+        abc = abcbalancer(i)
+        print("p: " + str(i) + "\ncount: " + str(abc))
+        if abc > max:
+            max = abc
+            ans = i
+    return ans
+    
 
-#find a, b, c --> max # of solutions
-
-
+print(solution(1000))
