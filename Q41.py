2dbccb144e6d874d7dade32677ae2be244142aec
Daeho  Kang <47322032+kdaeho1@users.noreply.github.com>
2024-02-23 15:33:55 +0900

    Add files via upload

--- Q41.py
+++ Q41.py
@@ -1,9 +1,31 @@
-def pan_generate(n):
-    #n = 9
-    pan_list = [i for i in range(1,n+1)]
-    current_list = pan_list
-    #return pan_list                #1 2 3 4 5 6 7 8 9  
-    for i in range(2, n):           # 2 3 4 5 6 7 8 9
-        for j in range(n-1):        # 0 1 2 3 4 5 6 7 8
-        current_list = pan_list[:n-i] + pan_list[:]
-print(pan_generate(9))
+import math
+
+def prime(num):
+    for i in range(2, int(math.sqrt(num)+1)):   #exclude 1 and num
+        if num/i == int(num/i):
+            return False
+    return True
+
+def get_next_permutation(string, n):
+    for i in range(n-2, -1, -1):
+        try:
+            if string[i] > string[i+1]:
+                target = sorted(string[i:], reverse = True)
+                new_num = target.pop(target.index(string[i])+1)
+                last_nums = ''
+                last_nums = last_nums.join(target)
+                return string[:i] + new_num + last_nums
+        except IndexError:
+            print(string)
+    return None
+string = "987654321"
+cand = string
+n = 9
+while not prime(int(cand)):
+    cand = get_next_permutation(cand, n)
+    
+    if not cand:
+        string = string[1:]
+        n -= 1
+        cand = get_next_permutation(string, n)
+print(cand)
